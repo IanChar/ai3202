9527061fa453495bcc710fff33609fe4c87797b6
Ian Char <ianchar4@gmail.com>
2015-11-09 19:23:40 -0700

    Fixed to have spaces

--- Assignment8/DataParser.py
+++ Assignment8/DataParser.py
@@ -1,5 +1,6 @@
 import numpy as np
 NUM_LETTERS = 26
+NUM_STATES = 27
 
 class DataParser(object):
     def __init__(self, filename):
@@ -7,10 +8,10 @@ class DataParser(object):
 
     def computeProbabilities(self):
         # P(E_t | X_t), stored matrix where row is x and column is e
-        evidenceProbs = np.matrix([[0 for _ in range(26)] for __ in range(26)])
+        evidenceProbs = np.matrix([[0 for _ in range(27)] for __ in range(27)])
         evidenceProbs = evidenceProbs.astype(float)
         # P(E_(t+1) | E_t), stored as matrix where row is x_t anc col is x_t+1
-        transitionProbs = np.matrix([[0 for _ in range(26)] for __ in range(26)])
+        transitionProbs = np.matrix([[0 for _ in range(27)] for __ in range(27)])
         transitionProbs = transitionProbs.astype(float)
         ev, states = self.parseInData()
         # Fill out evidence
@@ -22,6 +23,7 @@ class DataParser(object):
                     seen = 0
                 evidenceProbs[ord(prior) - 97, s - 97] = (float(seen + 1)
                         / (observed['count'] + NUM_LETTERS))
+        evidenceProbs[26, 26] = 1
         # Fill out state transitions
         for prior, observed in states.iteritems():
             for s in range(97, 123):
@@ -29,8 +31,23 @@ class DataParser(object):
                     seen = observed[chr(s)]
                 else:
                     seen = 0
-                transitionProbs[ord(prior) - 97, s - 97] = (float(seen + 1)
-                        / (observed['count'] + NUM_LETTERS))
+                if prior == "_":
+                    transitionProbs[26, s - 97] = (float(seen + 1)
+                            / (observed['count'] + NUM_STATES))
+                else:
+                    transitionProbs[ord(prior) - 97, s - 97] = (float(seen + 1)
+                            / (observed['count'] + NUM_STATES))
+            s = '_'
+            if s in observed.keys():
+                seen = observed[s]
+            else:
+                seen = 0
+            if prior == "_":
+                transitionProbs[26, 26] = (float(seen + 1)
+                        / (observed['count'] + NUM_STATES))
+            else:
+                transitionProbs[ord(prior) - 97, 26] = (float(seen + 1)
+                        / (observed['count'] + NUM_STATES))
         return evidenceProbs, transitionProbs
 
     def parseInData(self):
@@ -42,6 +59,7 @@ class DataParser(object):
         for i in range(97, 123):
             tmpEvidence[chr(i)] = {'count' : 0}
             tmpState[chr(i)] = {'count' : 0}
+        tmpState['_'] = {'count': 0}
 
         # Iterate over the lines of the file
         curr = f.readline().split(' ')
@@ -62,14 +80,13 @@ class DataParser(object):
                 else:
                     e[curr[1]] = 1
                 e['count'] += 1
-                # Update state
-                if nxt[0] != "_":
-                    s = tmpState[curr[0]]
-                    if nxt[0] in s.keys():
-                        s[nxt[0]] += 1
-                    else:
-                        s[nxt[0]] = 1
-                    s['count'] += 1
+            # Update state
+            s = tmpState[curr[0]]
+            if nxt[0] in s.keys():
+                s[nxt[0]] += 1
+            else:
+                s[nxt[0]] = 1
+            s['count'] += 1
             curr = nxt
             nxt = f.readline().split(' ')
         f.close()
@@ -78,4 +95,8 @@ class DataParser(object):
 if __name__ == '__main__':
     dp = DataParser('typos20.data')
     e, s = dp.computeProbabilities()
-    print s[ord('q') - 97, ord('u') - 97]
+    print s[ord('q') - 97]
+    a = 0
+    for i in s[ord('q') - 97]:
+        a += i
+    print s[ord('q') - 97].cumsum()
--- Assignment8/HiddenMarkovModel.py
+++ Assignment8/HiddenMarkovModel.py
@@ -9,12 +9,56 @@ class HiddenMarkovModel(object):
         self.ePtm, self.sPtm = dp.computeProbabilities()
 
     def getEProb(self, prior, subject):
-        return self.ePtm[ord(prior) - A_ASCII, ord(subject) - A_ASCII]
+        if prior == '_':
+            prior = 26
+        else:
+            prior = ord(prior) - A_ASCII
+        if subject == '_':
+            subject = 26
+        else:
+            subject = ord(subject) - A_ASCII
+        return self.ePtm[prior, subject]
 
     def getSProb(self, prior, subject):
-        return self.sPtm[ord(prior) - A_ASCII, ord(subject) - A_ASCII]
+        if prior == '_':
+            prior = 26
+        else:
+            prior = ord(prior) - A_ASCII
+        if subject == '_':
+            subject = 26
+        else:
+            subject = ord(subject) - A_ASCII
+        return self.sPtm[prior, subject]
+
+    def printEPtmTable(self):
+        for i in range(27):
+            if i == 26:
+                iChar = "_"
+            else:
+                iChar = chr(i + A_ASCII)
+            for j in range(27):
+                if j == 26:
+                    jChar = "_"
+                else:
+                    jChar = chr(j + A_ASCII)
+                print "P(", jChar, "|", iChar, ") = ", self.getEProb(iChar, jChar)
+
+    def printSPtmTable(self):
+        for i in range(27):
+            if i == 26:
+                iChar = "_"
+            else:
+                iChar = chr(i + A_ASCII)
+            for j in range(27):
+                if j == 26:
+                    jChar = "_"
+                else:
+                    jChar = chr(j + A_ASCII)
+                print "P(", jChar, "|", iChar, ") = ", self.getSProb(iChar, jChar)
 
 if __name__ == '__main__':
     hmm = HiddenMarkovModel('typos20.data')
-    print hmm.getEProb('a', 'a'), hmm.getEProb('a', 's'), hmm.getEProb('a', 'p')
-    print hmm.getSProb('q', 'u'), hmm.getSProb('q', 'z')
+    print "--------------Evidence Probabilties--------------"
+    hmm.printEPtmTable()
+    print "--------------Transition Probabilties--------------"
+    hmm.printSPtmTable()
